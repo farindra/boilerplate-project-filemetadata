3c3f34a0e892b89ed8cbfc7fb9f8fd64657074fc
Replit user <replituser@example.com>
2021-03-13 10:36:41 +0000

    first

--- server.js
+++ server.js
@@ -1,17 +1,45 @@
 var express = require('express');
 var cors = require('cors');
+const bodyParser= require('body-parser')
+
 require('dotenv').config()
 
 var app = express();
+const multer = require("multer");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "/uploads"));
+  }
+});
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post(
+  "/api/fileanalyse",
+  multer({ storage: diskStorage }).single("upfile"),
+  (req, res) => {
+    const file = req.file.path;
+    if (!file) {
+      res.status(400).send({
+        status: false,
+        data: "No File is selected.",
+      });
+    }
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    });
+  }
+);
 
 
 const port = process.env.PORT || 3000;
